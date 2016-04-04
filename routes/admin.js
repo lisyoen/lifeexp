fbeb75dfc1a2f7f109938d6dac1a31b289a40047
lisyoen <lisyoen@gmail.com>
2016-04-05 01:16:41 +0900

    add test files

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('admin page');
+  res.render('admin', {
+    title: '인생경험치 관리자 페이지',
+    count: 5
+  });
 });
 
 module.exports = router;
